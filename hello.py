5d39cd2355ad6bef3fc38f41036b3a58c3215e64
KingSelf <519715203@qq.com>
2018-11-10 15:49:38 +0800

    abc

---
+++ hello.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+#================================================================
+#   Copyright (C) 2018 MyCompany. All rights reserved.
+#   
+#   File Name  : hello.py
+#   Author     : Zhaokui
+#   Create Date: 2018-11-10 15:14
+#   Describle  : 
+#
+#================================================================
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+
